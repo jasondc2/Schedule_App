7d82601aa430ea2f02237f8eb96d979350992c0e
Jason Christensen <jasondchristensen@outlook.com>
2016-11-02 21:01:04 -0600

    added AssignTest class

--- app/src/main/java/com/example/jason/schedule_app/test/AssignTest.java
+++ app/src/main/java/com/example/jason/schedule_app/test/AssignTest.java
@@ -11,13 +11,17 @@ import static junit.framework.Assert.assertEquals;
  * Created by jason on 11/2/2016.
  */
 
-public class AssignTest {
+public class AssignTest extends InstrumentationTestCase {
+
+    assignment assign;
 
     public void checkPointType() throws Exception {
-
+        assert(assign.getPointValue(7, 8) == 3.8);
+        assert(assign.getPointValue(4, 10) == 3.2);
     }
 
     public void checkViewType() throws Exception {
-
+        assert(assign.viewType(true) == true);
+        assert(assign.viewType(false) == false);
     }
 }
