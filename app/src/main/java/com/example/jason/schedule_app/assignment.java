6b7eecc97f9a3c1c6b4b94e5dff695a16c3a57b7
Jason Christensen <jasondchristensen@outlook.com>
2016-11-02 18:41:48 -0600

    added the assignment class, getPointValue method, and getViewType method.

---
+++ app/src/main/java/com/example/jason/schedule_app/assignment.java
@@ -0,0 +1,18 @@
+package com.example.jason.schedule_app;
+
+/**
+ * Created by jason on 11/2/2016.
+ */
+
+public class assignment {
+    private float pointValue;
+    private boolean isList;
+
+    public float getPointValue() {
+        return pointValue;
+    }
+
+    public boolean getViewType() {
+        return isList;
+    }
+}
