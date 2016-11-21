214312fd2ff896ec42e259d2f195dec8efd9f040
Tiffany Gohnert <tiffanygohnert@gmail.com>
2016-11-21 15:04:41 -0800

    List View works

--- app/src/main/java/com/example/jason/schedule_app/MainActivity.java
+++ app/src/main/java/com/example/jason/schedule_app/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.jason.schedule_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Get the view from activity_main.xml
+
+        // Locate the button in activity_main.xml
+        button = (Button) findViewById(R.id.buttonlist);
+
+        // Capture button clicks
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MainActivity.this,listActivity.class);
+                startActivity(myIntent);
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+       // getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
     }
 }
+
