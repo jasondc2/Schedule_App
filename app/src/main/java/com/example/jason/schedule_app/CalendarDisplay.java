e454237c8ee7eb10ad833b3fe749b4c30ea8ac74
Sabrina Bell <skibri@yahoo.com>
2016-12-05 15:53:13 -0800

    dueDate completed (1 weird bug) 3 warnings in calendarDisplay, but mostly working

--- app/src/main/java/com/example/jason/schedule_app/CalendarDisplay.java
+++ app/src/main/java/com/example/jason/schedule_app/CalendarDisplay.java
@@ -1,18 +1,25 @@
 package com.example.jason.schedule_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class CalendarDisplay extends AppCompatActivity {
     //initializes on current month,year
-    //uses Tiffany's com.example.jason.schedule_app.SAtest.queue to load assignments that are in this month, year... so check for those
+    //uses Tiffany's queue to load assignments that are in this month, year... so check for those
+    //put them in an array
     TextView Month;
     Calendar calendar;
     Button prevMonth;
@@ -33,14 +40,54 @@ public class CalendarDisplay extends AppCompatActivity {
     TextView Friday;
     TextView Saturday;
 
+    boolean reset; //are we resetting the view?
+    Queue<assignment> AssignmentList;
+    List<List<assignment>> AssignmentDay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        Intent intent = getIntent();
+        AssignmentList = (PriorityQueue<assignment>)intent.getSerializableExtra("AssignmentQueue");
+        //now we have all of the assignments
+        AssignmentDay = new ArrayList<>();
+        for (int i = 0; i < calendar.getActualMaximum(Calendar.DATE); i++){
+            //make each AssignmentDay array a mini arraylist...
+            ArrayList temp = new ArrayList();
+            AssignmentDay.add(temp);//of type assignmentMain //should be uppercase
+            //if MONTH CHANGES, CLEAR EACH sublist's assignments
+            //we've added each day's lists... now we can add the assignments in each day by going
+            //to AssignmentDay
+        }
+
         Month = (TextView) findViewById(R.id.Month);//initially says 'Month'
         calendar = Calendar.getInstance(); //current date... from here we will set up the month
+
+
         prevMonth = (Button) findViewById(R.id.prevMonth);
         nextMonth = (Button) findViewById(R.id.nextMonth);
+
+        prevMonth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setMonth(-1);
+                setMonthHeader();
+                resetDisplay();//make sure we don't have old data
+                setDisplay();
+            }
+        });
+
+        nextMonth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setMonth(1);
+                setMonthHeader();
+                resetDisplay();
+                setDisplay();
+            }
+        });
 
         weekOne = findViewById(R.id.week1);
         weekTwo = findViewById(R.id.week2);
@@ -49,7 +96,13 @@ public class CalendarDisplay extends AppCompatActivity {
         weekFive = findViewById(R.id.week5);
         weekSix = findViewById(R.id.week6);
 
+    }
 
+    //view next or previous month
+    public void setMonth(int num){
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        calendar.add(Calendar.MONTH, num);
+        //first of the next month or previous month
     }
 
     //displays month name
@@ -62,14 +115,14 @@ public class CalendarDisplay extends AppCompatActivity {
     //right now this is just displaying the month... add the colorDate function to put priorities in
     public void setDisplay(){
         calendar.set(Calendar.DATE, 1);
-        String currentDay = new SimpleDateFormat("EEEE", Locale.US).format(calendar);//first day of the month
+        String currentDay = new SimpleDateFormat("EEEE", Locale.US).format(calendar);//1st day of mo
 
         int week = 1; //which week row we are on
         int dayNum = 1; //the date in number form
 
         View tempWeek; //lets us assign one week at a time
 
-        while (week < 7 && dayNum != calendar.getActualMaximum(Calendar.DATE)) {//last day of month?
+        while (week < 7 && dayNum != calendar.getActualMaximum(Calendar.DATE)) {//last day of month
 
             if (week == 1) {
                 tempWeek = weekOne;
@@ -138,9 +191,18 @@ public class CalendarDisplay extends AppCompatActivity {
                     break;
             }
 
-            //day.setText(Integer.toString(dayNum));
             try {
-                day.setText(String.format(Locale.US, "%1$02d", dayNum));
+                if (reset){
+                    day.setText(" ");
+                }
+                else {
+                    day.setText(String.format(Locale.US, "%1$d", dayNum));//00d?
+                    List tempAssign = new ArrayList();
+                    //AssignmentDay.add(dayNum, tempListof assigns )
+                    //set color
+                    //priority queue stuff // put in any dates
+                    //AssignmentList
+                }
             }
             catch(NullPointerException ex){
                 //HMM
@@ -154,16 +216,51 @@ public class CalendarDisplay extends AppCompatActivity {
         }
     }
 
-    //view next or previous month
-    public void setMonth(int num){
-        calendar.set(Calendar.DAY_OF_MONTH, 1);
-        calendar.add(Calendar.MONTH, num);
-        //first of the next month or previous month
+    public void resetDisplay(){
+        reset = true;
+        setDisplay();
+        AssignmentList.clear();
+        for (int i = 0; i < calendar.getActualMaximum(Calendar.DATE); i++){
+            ArrayList temp = new ArrayList();
+            AssignmentDay.add(temp); //reset the array for the new month
+        }
+        reset = false;
     }
 
-    public void colorDate(){}//color each date according to highest priority assignment
+    public void colorDate(){}//color each date according to highest priority assignmentMain
 
+    //What's the score range???
 
+    //having clicked a date
+    public void onClick(View v){
+        //open popup
+        PopupWindow popUpWindow = new PopupWindow(this);
+        //it's a scrollable list screen
+        //WORK HERE
+        //view assignments from least to most important
+        //edit button next to each?
+        //complete buton next to each
+        //if complete, delete.
+        //
+    }
 
-    //2 buttons... <- and ->    on their clicks, switch months... switch months
+    //for int i = 0; i < Calendar.maxMonth...; i++
+    //Date =assignmentlist.get(i).getDate?
+    //if date.month == the month we need
+    //and date.year == the year we need
+    //add to date.day's list
+    //continue through list
+
+    //WHEN WE GO TO OTHER PAGES, SHARE THE ARRAY AND MAKE NECESSARY CHANGES (DELETE... or if edited, it
+    //will delete and add)
+
+    //MENU BUTTON
+    //Intent intent = new intent();
+    //intent.putExtra("AssignmentList", assignmentList);
+    //if add, go to addassign
+    //if listview, go to listview
+    //intent.putExtra(flag, C)//from Calendar maybe
+
+    //MAKE PRIORITY QUEUE SERIALIZABLE!!!!!!!!
 }
+//SABRINA BELL
--- app/src/main/java/com/example/jason/schedule_app/DueDate.java
+++ app/src/main/java/com/example/jason/schedule_app/DueDate.java
@@ -1,48 +1,57 @@
 package com.example.jason.schedule_app;
+import android.util.Log;
 
 import java.util.Calendar;
+import java.util.Locale;
 
-/**
- * Created by Sabrina on 11/2/16.
- */
-
+//for the assignments' due dates... can be used while putting them in the queue maybe?
 public class DueDate {
-    Calendar dueDate;
-    private int year;
-    private int month;
-    private int day;
-    private int hour;
-    private int minute;
-    private int AM_PM;
+    private Calendar dueDate;
 
     DueDate() {
-
+        dueDate = Calendar.getInstance(Locale.US); //defaults on current day
     }
 
     public void setDate(int year, int month, int day, int hour, int minute, int AM_PM) {
+        if (hour > 12){
+            Log.e("DueDate", "Impossible time");
+            //throw error?
+        }
         if (AM_PM == Calendar.PM) {
             dueDate.set(year, month, day, hour + 12, minute);
+            Log.i("dueDate", "actual time is" + hour);
             return;
         }
         dueDate.set(year, month, day, hour, minute);
-        return;
     }
 
-    public void getDueDate() {
+    public boolean isPM() {
         if(dueDate.get(Calendar.HOUR_OF_DAY) > 12){
-            return ;
+            return true;
         }
+        Log.i("dueDate", "AM");
+        return false;
+    }
+
+    public Calendar getDueDate() {
+        if((dueDate.get(Calendar.HOUR_OF_DAY) > 12) &&
+                (dueDate.get(Calendar.AM_PM) == Calendar.PM)){ //weird bug
+            dueDate.set(Calendar.HOUR_OF_DAY, dueDate.get(Calendar.HOUR_OF_DAY) - 12);
+        }
+        return dueDate;//might need work here
     }
 
     public int compareCurrentDate() {
-        //compare with current date
-        return 0;
+        return dueDate.compareTo(Calendar.getInstance()); //compare to current day
+        //+# means we still got time, -# means overdue
+
     }
 
     //compare dates to prioritize
-    public int compareDate(DueDate date) {
+    public int compareDate(Calendar date) {
+        return dueDate.compareTo(date);
         //compare with another date
-        return 0;
+        //+# means first is bigger, -# means second is bigger, I think
     }
 }
-
+//SABRINA BELL
